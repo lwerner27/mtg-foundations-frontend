0ac768888c106918fdf81294eeb5b6e4cf318eec
Lucas Werner <lwerner27@gmail.com>
2024-09-16 19:06:21 -0500

    Started working on the layout for deck creation page.

--- src/routes/decks/create/[id]/+page.svelte
+++ src/routes/decks/create/[id]/+page.svelte
@@ -4,8 +4,14 @@
 	let { commander } = data;
 </script>
 
-<h1>{commander.name}</h1>
-<img
-	src={`${commander.image_uris.png ? commander.image_uris.png : commander.image_uris.normal}`}
-	alt={`${commander.name}`}
-/>
+<div class="container mx-auto">
+	<section>
+		<div class="grid grid-cols-3 gap-7">
+			<img
+				class="w-3/5"
+				src={`${commander.image_uris.png ? commander.image_uris.png : commander.image_uris.normal}`}
+				alt={`${commander.name}`}
+			/>
+		</div>
+	</section>
+</div>
