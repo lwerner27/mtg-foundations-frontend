6ad05082ba42803e165e3843fbb254ca9ad88aba
Lucas Werner <lwerner27@gmail.com>
2024-09-25 22:38:49 -0500

    Refactored grid layout.

--- src/routes/decks/create/[id]/+page.svelte
+++ src/routes/decks/create/[id]/+page.svelte
@@ -8,41 +8,23 @@
 	let black = 0;
 	let red = 0;
 	let green = 0;
-	let zerocmc;
-	let onecmc;
-	let twocmc;
-	let threecmc;
-	let fourcmc;
-	let fivecmc;
-	let sixcmc;
-	let sevencmc;
-	let eightpluscmc;
+	let zerocmc = 0;
+	let onecmc = 0;
+	let twocmc = 0;
+	let threecmc = 0;
+	let fourcmc = 0;
+	let fivecmc = 0;
+	let sixcmc = 0;
+	let sevenpluscmc = 0;
 </script>
 
 <div class="container mx-auto">
 	<section>
-		<div class="grid md:grid-cols-3 gap-7">
+		<div class="grid md:grid-cols-4 gap-7">
 			<div class="col mt-4">
 				<img
 					src={`${commander.image_uris.png ? commander.image_uris.png : commander.image_uris.normal}`}
 					alt={`${commander.name}`}
-				/>
-			</div>
-			<div class="form-control mt-4">
-				<h4 class="h4">Mana Curve</h4>
-				<input class="input mt-5" type="number" bind:value={zerocmc} placeholder="0 CMC Spells" />
-				<input class="input mt-5" type="number" bind:value={onecmc} placeholder="1 CMC Spells" />
-				<input class="input mt-5" type="number" bind:value={twocmc} placeholder="2 CMC Spells" />
-				<input class="input mt-5" type="number" bind:value={threecmc} placeholder="3 CMC Spells" />
-				<input class="input mt-5" type="number" bind:value={fourcmc} placeholder="4 CMC Spells" />
-				<input class="input mt-5" type="number" bind:value={fivecmc} placeholder="5 CMC Spells" />
-				<input class="input mt-5" type="number" bind:value={sixcmc} placeholder="6 CMC Spells" />
-				<input class="input mt-5" type="number" bind:value={sevencmc} placeholder="7 CMC Spells" />
-				<input
-					class="input mt-5"
-					type="number"
-					bind:value={eightpluscmc}
-					placeholder="8+ CMC Spells"
 				/>
 			</div>
 			<div class="form-control mt-4">
@@ -71,9 +53,50 @@
 					<span>Green Sources</span>
 					<input class="input" type="number" bind:value={green} />
 				</label>
+			</div>
+			<div class="col-span-2 form-control mt-4">
+				<h4 class="h4">Mana Curve</h4>
+				<div class="grid md:grid-cols-2 gap-7">
+					<div class="col">
+						<label class="label mt-2">
+							<span>0 CMC Spells</span>
+							<input class="input mt-5" type="number" bind:value={zerocmc} />
+						</label>
+						<label class="label mt-2">
+							<span>1 CMC Spells</span>
+							<input class="input mt-5" type="number" bind:value={onecmc} />
+						</label>
+						<label class="label mt-2">
+							<span>2 CMC Spells</span>
+							<input class="input mt-5" type="number" bind:value={twocmc} />
+						</label>
+						<label class="label mt-2">
+							<span>3 CMC Spells</span>
+							<input class="input mt-5" type="number" bind:value={threecmc} />
+						</label>
+					</div>
+					<div class="col">
+						<label class="label mt-2">
+							<span>4 CMC Spells</span>
+							<input class="input mt-5" type="number" bind:value={fourcmc} />
+						</label>
+						<label class="label mt-2">
+							<span>5 CMC Spells</span>
+							<input class="input mt-5" type="number" bind:value={fivecmc} />
+						</label>
+						<label class="label mt-2">
+							<span>6 CMC Spells</span>
+							<input class="input mt-5" type="number" bind:value={sixcmc} />
+						</label>
+						<label class="label mt-2">
+							<span>7+ CMC Spells</span>
+							<input class="input mt-5" type="number" bind:value={sevenpluscmc} />
+						</label>
+					</div>
+				</div>
 				<button
 					type="button"
-					class="btn bg-gradient-to-br variant-gradient-primary-tertiary w-full mt-4">SAVE</button
+					class="btn bg-gradient-to-br variant-gradient-primary-tertiary w-full mt-9">SAVE</button
 				>
 			</div>
 		</div>
